414d5ce048f6bc5f3b10fbc2caf67a012420edee
Hayley <hayley.tong@sainsburys.co.uk>
2025-01-21 15:04:15 +0000

    adds eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,70 @@
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  pluginReactConfig,
+  {
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    },
+    rules: {
+      'no-restricted-imports': [
+        'error',
+        {
+          name: '@playwright/test',
+          message: 'Please use ./tests/lib/fixture instead.'
+        },
+        {
+          name: '@testing-library/react',
+          message: 'Please use @/lib/@testing-library'
+        },
+        {
+          name: '@sainsburys-tech/bolt',
+          message: 'Please use @sainsburys-tech/fable instead'
+        }
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ],
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: {
+            attributes: false
+          }
+        }
+      ]
+    }
+  },
+  {
+    ignores: ['.next/', 'extension', 'playwright-report', 'newrelic.js', '*.mjs', 'test-results']
+  }
+]
