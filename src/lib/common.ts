4da798c2bc3c73b960b13f217988b0f0ef387ed8
Hayley <hayley.tong@sainsburys.co.uk>
2025-01-27 18:26:06 +0000

    add bundlewatch to prepush

--- src/lib/common.ts
+++ src/lib/common.ts
@@ -3,14 +3,3 @@ export enum Brands {
   tu = 'tu',
   habitat = 'habitat'
 }
-
-export const Styles = ({ brand }: { brand: Brands }) => {
-  switch (brand) {
-    case Brands.argos:
-      return 'argos'
-    case Brands.habitat:
-      return 'argos'
-    case Brands.tu:
-      return 'argos'
-  }
-}
