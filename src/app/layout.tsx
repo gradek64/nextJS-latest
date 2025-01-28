e5c5e737a41f464cd13e044ec8daf16a782421fa
Hayley <hayley.tong@sainsburys.co.uk>
2025-01-28 13:57:18 +0000

    Add SSR for fable styles and style index page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { StylesManager } from '@sainsburys-tech/nextjs-support'
 import { ThemeProvider } from '@sainsburys-tech/theme-provider'
 import { Brands } from '../lib/common'
 import { Styles } from './styles'
@@ -15,7 +16,10 @@ export default function RootLayout({
         <Styles />
       </head>
       <body>
-        <ThemeProvider brand={Brands.argos}>{children}</ThemeProvider>
+        {/* StylesManager is used for SSRing fable styles */}
+        <StylesManager>
+          <ThemeProvider brand={Brands.argos}>{children}</ThemeProvider>
+        </StylesManager>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,15 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <Link href='/wishlist'>Go to Wishlist </Link>
+    <div className='ds-justify-center ds-items-center ds-flex ds-flex-col'>
+      <main className='ds-w-[500px] ds-mt-18'>
+        <h1 className='ds-mb-4'>Pages</h1>
+        <Link href='/wishlist'>
+          <div className='ds-p-4 ds-shadow hover:ds-bg-blue hover:ds-text-white'>
+            <h2 className='ds-c-display-1'>Wishlist</h2>
+            <p className='ds-m-0 ds-text-sm'>/wishlist</p>
+          </div>
+        </Link>
       </main>
     </div>
   )
